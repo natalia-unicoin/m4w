658438e8298c6138ab2578ebb9fba3913473f14e
Natalia Duarte <Naty@MacBook-Pro-de-Ale.local>
2026-03-20 11:04:13 -0300

    style(FourWays): center image closer to the middle for box 2

--- src/components/FourWays.tsx
+++ src/components/FourWays.tsx
@@ -32,7 +32,7 @@ const FourWays = () => {
             description: "You install a personal wealth system that organizes your money and strengthens your decision-making.",
             color: "#B79DEC", // M4W Light Purple
             image: '/box2.jpg',
-            imagePosition: 'center 20%'
+            imagePosition: 'center 40%'
         },
         {
             title: "Investment Discipline",
