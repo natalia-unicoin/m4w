b4e8f0788da336673c652852b00e81783a3d23da
Natalia Duarte <Naty@MacBook-Pro-de-Ale.local>
2026-03-20 11:22:21 -0300

    style(FourWays): integrate user-edited box2.jpg and disable css framing

--- src/components/FourWays.tsx
+++ src/components/FourWays.tsx
@@ -31,8 +31,7 @@ const FourWays = () => {
             title: "Structure",
             description: "You install a personal wealth system that organizes your money and strengthens your decision-making.",
             color: "#B79DEC", // M4W Light Purple
-            image: '/box2.jpg',
-            imagePosition: 'right 40%'
+            image: '/box2.jpg'
         },
         {
             title: "Investment Discipline",
